df4723f02474fd212d31846118e63fa121ea8123
EmirlanDogdurbaev <jigit38@gmail.com>
2024-04-17 16:21:20 +0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Layout from "./components/Layout/Layout.jsx";
 import { Route, Routes } from "react-router-dom";
+import AudioCard from "./components/AudioCard/AudioCard.jsx";
 
 function App() {
   return (
     <>
       <Layout>
         <Routes>
-          <Route path="/" exact component={App} />
+          <Route path="/" exact element={<AudioCard />}/>
+            <Route path="/test" exact element={<AudioCard/>} />
         </Routes>
       </Layout>
     </>
