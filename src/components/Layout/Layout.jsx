179e56d6bbf22068a6f55e31862733dd141580ec
darkbala <rustam.samsaev@gmail.com>
2024-04-17 21:47:10 +0600

    fixes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,6 @@
-// import FlexCard from "../FlexCard/FlexCard";
 import FlexCard from "../FlexCard/FlexCard";
 import Navbar from "../Navbar/Navbar";
 
-// eslint-disable-next-line react/prop-types
 const Layout = ({ children }) => {
   return (
     <main>
