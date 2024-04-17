d3f0b99f29ae73dc45c9660f97c237550ad3d88c
Aidarbek Kurmankozhoev <aidarbek.kurmankojoev.pls@gmail.com>
2024-04-17 21:09:48 +0600

    filmdetail

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Films from "./components/Films/Films.jsx";
 import Layout from "./components/Layout/Layout.jsx";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Register from "./components/Auth/Register/Register.jsx";
@@ -6,8 +5,10 @@ import Login from "./components/Auth/Login/Login.jsx";
 import Books from "./components/Books/Books.jsx";
 import Homepage from "./pages/Homepage/Homepage.jsx";
 import Bookpage from "./pages/Bookpage/Bookpage.jsx";
+import Filmpage from "./pages/Filmpage/Filmpage.jsx";
 import Courses from "./components/Courses/Courses.jsx";
 import BookDetail from "./components/BookDetail/BookDetail.jsx";
+import FilmDetail from "./components/FilmDetail/FilmDetail.jsx";
 
 function App() {
   return (
@@ -24,7 +25,8 @@ function App() {
             <Route path="/" index element={<Homepage />} />
             <Route path="/bookpage" element={<Bookpage />} />
             <Route path="/bookdetail" element={<BookDetail />} />
-            <Route path="/films" element={<Films />} />
+            <Route path="/filmdetail" element={<FilmDetail />} />
+            <Route path="/films" element={<Filmpage />} />
             <Route path="/books" element={<Books />} />
             <Route path="/courses" element={<Courses />} />
             <Route path="*" element={<Navigate to="/" />} />
---
+++ src/components/FilmDetail/FilmDetail.jsx
@@ -0,0 +1,53 @@
+import classes from "./FilmDetail.module.scss";
+import { Link } from "react-router-dom";
+
+const FilmDetail = () => {
+    const dynamic_text = `Я был правозащитником, а бывших правозащитников не бывает» — на
+    вопросы RFI ответил заместитель председателя кабинета министров
+    Кыргызстана Эдиль Байсалов «Перелом в сознании произошел», — завил
+    в интервью RFI  по поводу прав женщин заместитель председателя
+    кабинета министров Кыргызстана Эдиль Байсалов, который прибыл в Париж
+    в связи с празднованиями в честь 75-летия принятия Всеобщей декларации
+    прав человека. Он также ответил на вопросы об экологических правах,
+    борьбе с коррупцией и об ограничивающих свободу гражданского общества
+    законопроектах. Я был правозащитником, а бывших правозащитников
+    не бывает» — на вопросы RFI ответил заместитель председателя кабинета
+    министров Кыргызстана Эдиль Байсалов «Перелом в сознании
+    произошел», — завил в интервью RFI  по поводу прав женщин заместитель
+    председателя кабинета министров Кыргызстана Эдиль Байсалов, который
+    прибыл в Париж в связи с празднованиями в честь 75-летия принятия
+    Всеобщей декларации прав человека. Он также ответил на вопросы об
+    экологических правах, борьбе с коррупцией и об ограничивающих свободу
+    гражданского общества законопроектах.`;
+
+    return (  
+        <div className={classes.wrap}>
+            <div className={classes.left}>
+                <video   className={classes.leftArea} autoPlay controls>
+                <source src="https://cdn.pixabay.com/video/2023/12/10/192687-893427276_large.mp4" allowfullscreen type="video/mp4"/>
+                </video>
+            <Link to="#">
+                <div className={classes.btn}>
+                <p className={classes.btnText}>Загрузить видео</p>
+                </div>
+            </Link>
+            </div>
+            <div className={classes.right}>
+            <p className={classes.rText}>{dynamic_text}</p>
+            <div className={classes.rbtns}>
+                <div className={classes.rsmall}>
+                <p>Скачать .txt</p>
+                </div>
+                <div className={classes.rsmall}>
+                <p>Скачать .pdf</p>
+                </div>
+                <div className={classes.rsmall}>
+                <p>Копировать</p>
+                </div>
+            </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default FilmDetail;
