b5f75b4cd23423898c414deb5afe82851c6d81c3
darkbala <rustam.samsaev@gmail.com>
2024-04-18 01:45:07 +0600

    remove latest functional

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import cl from "./BookDetail.module.scss";
 import { Link } from "react-router-dom";
-import clipboard from "clipboard-copy";
 
 function BookDetail() {
   const dynamic_text = `Я был правозащитником, а бывших правозащитников не бывает» — на
@@ -22,20 +21,6 @@ function BookDetail() {
           экологических правах, борьбе с коррупцией и об ограничивающих свободу
           гражданского общества законопроектах.`;
 
-  const handleDownloadTxt = () => {
-    const element = document.createElement("a");
-    const file = new Blob([dynamic_text], { type: "text/plain" });
-    element.href = URL.createObjectURL(file);
-    element.download = "text.txt";
-    document.body.appendChild(element);
-    element.click();
-  };
-
-  const handleCopyText = () => {
-    clipboard(dynamic_text);
-    alert("Текст скопирован в буфер обмена ");
-  };
-
   return (
     <div className={cl.wrap}>
       <div className={cl.left}>
@@ -52,12 +37,14 @@ function BookDetail() {
       <div className={cl.right}>
         <p className={cl.rText}>{dynamic_text}</p>
         <div className={cl.rbtns}>
-          <div className={cl.rsmall} onClick={handleDownloadTxt}>
-            <p>Скачать .txt</p>
+          <div className={cl.rsmall}>
+            <p>Кыргызский</p>
           </div>
-
-          <div className={cl.rsmall} onClick={handleCopyText}>
-            <p>Копировать</p>
+          <div className={cl.rsmall}>
+            <p>Русский</p>
+          </div>
+          <div className={cl.rsmall}>
+            <p>Английский</p>
           </div>
         </div>
       </div>
