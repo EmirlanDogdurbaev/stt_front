4a4799606c9e52c8b57e7aad636f6bf0a9a59fe3
darkbala <rustam.samsaev@gmail.com>
2024-04-17 15:57:17 +0600

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import Layout from "./components/Layout/Layout.jsx";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-    return (
-        <>
-          <Layout>
-
-              <Routes>
-                  <Route path="/" exact component={App} />
-              </Routes>
-          </Layout>
-        </>
-    )
+  return (
+    <>
+      <Layout>
+        <Routes>
+          <Route path="/" exact component={App} />
+        </Routes>
+      </Layout>
+    </>
+  );
 }
 
-export default App
+export default App;
