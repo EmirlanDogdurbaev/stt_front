81f8a050469cd65e48c53551e0b381b101e54ed1
EmirlanDogdurbaev <jigit38@gmail.com>
2024-04-17 16:34:22 +0600

    fix audio card

--- src/components/AudioCard/AudioCard.jsx
+++ src/components/AudioCard/AudioCard.jsx
@@ -1,13 +1,5 @@
 const AudioCard = () => {
-<<<<<<< HEAD
-  return (
-    <article>
-      <h2>Lorem.Ò</h2>
-    </article>
-  );
-};
-export default AudioCard;
-=======
+
     return (
         <article>
             <h2>Lorem.Ò wdsfsdfdsf</h2>
@@ -15,4 +7,4 @@ export default AudioCard;
     )
 }
 export default AudioCard;
->>>>>>> 5386c86 (merge)
+
