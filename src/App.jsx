c8b7d3bf118347b01976a610d6a9f9e251c8453c
EmirlanDogdurbaev <jigit38@gmail.com>
2024-04-17 17:33:00 +0600

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
 import Films from "./components/Films/Films.jsx";
 import Layout from "./components/Layout/Layout.jsx";
-import { Navigate, Route, Routes } from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import AudioCard from "./components/AudioCard/AudioCard.jsx";
 import Register from "./components/Auth/Register/Register.jsx";
 import Login from "./components/Auth/Login/Login.jsx";
 import Books from "./components/Books/Books.jsx";
 
+
 function App() {
-  return (
-    <>
-      <Layout>
-        {!localStorage.getItem("token") ? (
-          <Routes>
-            <Route path="/" index element={<AudioCard />} />
-            <Route path="/test" element={<AudioCard />} />
-            <Route path="/films" element={<Films />} />
-            <Route path="/books" element={<Books />} />
-          </Routes>
-        ) : (
-          <Routes>
-            <Route path="/" element={<Navigate to="/login" />} />
-            <Route path="/register" index element={<Register />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        )}
-      </Layout>
-    </>
-  );
+    return (
+        <>
+            <Layout>
+                {!localStorage.getItem("token")  ? (
+                    <Routes>
+                        <Route path="/" index element={<AudioCard/>}/>
+                        <Route path="/test" element={<AudioCard/>}/>
+                        <Route path="/films" element={<Films/>}/>
+                        <Route path="/books" element={<Books/>}/>
+                        <Route path="*" element={<Navigate to="/" />} />
+
+                    </Routes>
+                ) : (
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/login" />} />
+                        <Route path="/signup" index element={<Register/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                    </Routes>
+                )}
+            </Layout>
+        </>
+    )
+        ;
 }
 
 export default App;
