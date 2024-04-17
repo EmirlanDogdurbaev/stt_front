182a86d22ac1c986dfab27baee0ca76911085591
Aidarbek Kurmankozhoev <aidarbek.kurmankojoev.pls@gmail.com>
2024-04-17 16:27:58 +0600

    films added

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" index element={<AudioCard />} />
           <Route path="/test" element={<AudioCard />} />
+          <Route path="/films" element={<Films/>}/>
         </Routes>
       </Layout>
     </>
