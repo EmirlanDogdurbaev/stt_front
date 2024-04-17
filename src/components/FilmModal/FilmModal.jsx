d25af335e272d09d2af8635acb167856f33fc151
darkbala <rustam.samsaev@gmail.com>
2024-04-18 05:25:21 +0600

    done func add new film

--- src/components/FilmModal/FilmModal.jsx
+++ src/components/FilmModal/FilmModal.jsx
@@ -1,18 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import cl from "./FilmModal.module.scss";
 
 function FilmModal(props) {
+  const [filmData, setFilmData] = useState({
+    name: "",
+    photo: null,
+    video: null,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    setFilmData({
+      ...filmData,
+      [name]: files ? files[0] : value, // Если это файл, сохраняем объект файла, иначе сохраняем значение
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData();
+      formData.append("title", filmData.name);
+      formData.append("image", filmData.photo);
+      formData.append("file", filmData.video);
+
+      const response = await axios.post(
+        "http://192.168.54.19:8000/movies/",
+        formData
+      );
+      console.log("Film added successfully:", response.data);
+      // Дополнительная логика после успешного добавления фильма, например, закрытие модального окна
+    } catch (error) {
+      console.error("Error adding film:", error);
+      // Обработка ошибки при добавлении фильма
+    }
+  };
+
   return (
     <div
       className={cl.wrap}
       style={{
-        display: props.isModalActive ? "flex" : "none", // Изменено условие для отображения компонента
+        display: props.isModalActive ? "flex" : "none",
       }}
     >
-      <form action="#" className={cl.content}>
-        <input type="text" placeholder="Name of film" />
-        <input type="file" accept="image/*" placeholder="Photo of film" />
-        <input type="file" accept="video/mp4" placeholder="MP4 of film" />
+      <form onSubmit={handleSubmit} className={cl.content}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name of film"
+          value={filmData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="file"
+          name="photo"
+          accept="image/*"
+          onChange={handleChange}
+        />
+        <input
+          type="file"
+          name="video"
+          accept="video/mp4"
+          onChange={handleChange}
+        />
         <button type="submit">Submit</button>
 
         <p
