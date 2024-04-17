dd20401d5920f057faa1b96f1167240dc2abd74e
darkbala <rustam.samsaev@gmail.com>
2024-04-17 23:54:47 +0600

    fix home color

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,21 +1,21 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import cl from "./Book.module.scss";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Book(props) {
-    // eslint-disable-next-line react/prop-types
-    const {imageUrl, author, name} = props;
+  // eslint-disable-next-line react/prop-types
+  const { imageUrl, author, name } = props;
 
-    return (
-        <Link to="/bookdetail">
-            <div className={cl.wrap}>
-                <img src={imageUrl} alt="photo"/>
-                <p className={cl.authorText}>{author}</p>
-                <p className={cl.nameText}>{name}</p>
-            </div>
-        </Link>
-    );
+  return (
+    <Link to="/bookdetail">
+      <div className={cl.wrap}>
+        <img src={imageUrl} alt="photo" />
+        <p className={cl.authorText}>{author}</p>
+        <p className={cl.nameText}>{name}</p>
+      </div>
+    </Link>
+  );
 }
 
 export default Book;
