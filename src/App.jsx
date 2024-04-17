5b2db3900b04598d8a3b730cec0d7e98fb362f39
darkbala <rustam.samsaev@gmail.com>
2024-04-17 22:01:09 +0600

    add Course details

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-import Films from "./components/Films/Films.jsx";
 import Layout from "./components/Layout/Layout.jsx";
-import {Navigate, Route, Routes} from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Register from "./components/Auth/Register/Register.jsx";
 import Login from "./components/Auth/Login/Login.jsx";
 import Books from "./components/Books/Books.jsx";
 import FlexCard from "./components/FlexCard/FlexCard.jsx";
 import Homepage from "./pages/Homepage/Homepage.jsx";
+import Filmpage from "./pages/Filmpage/Filmpage.jsx";
 
 function App() {
-    return (
-        <>
-            <Layout>
-                {localStorage.getItem("token") ? (
-                    <Routes>
-                        <Route path="/register"  element={<Register/>}/>
-                        <Route path="/login" element={<Login/>}/>
-                    </Routes>
-                ) : (
-                    <Routes>
-                        <Route path="/" index element={<Homepage/>}/>
-                        <Route path="/test" element={<FlexCard/>}/>
-                        <Route path="/films" element={<Films/>}/>
-                        <Route path="/books" element={<Books/>}/>
-                        <Route path="/register" element={<Register/>}/>
-                        <Route path="/login" element={<Login/>}/>
-                        <Route path="*" element={<Navigate to="/"/>}/>
-
-                    </Routes>
-                )}
-            </Layout>
-        </>
-    );
+  return (
+    <>
+      <Layout>
+        {localStorage.getItem("token") ? (
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/" index element={<Homepage />} />
+            <Route path="/test" element={<FlexCard />} />
+            <Route path="/films" element={<Filmpage />} />
+            <Route path="/books" element={<Books />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        )}
+      </Layout>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/CourseDetail/CourseDetail.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import cl from "./CourseDetail.module.scss";
+
+function CourseDetail() {
+  return <div>CourseDetail</div>;
+}
+
+export default CourseDetail;
