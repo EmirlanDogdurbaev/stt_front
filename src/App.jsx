85116b1e52efd1471a67d71c47b3d33c4d9c5414
EmirlanDogdurbaev <jigit38@gmail.com>
2024-04-17 16:54:19 +0600

    added merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 import Films from "./components/Films/Films.jsx";
 import Layout from "./components/Layout/Layout.jsx";
-import { Route, Routes } from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import AudioCard from "./components/AudioCard/AudioCard.jsx";
+import Register from "./components/Auth/Register/Register.jsx";
+import Login from "./components/Auth/Login/Login.jsx";
 import Books from "./components/Books/Books.jsx";
 
 
 function App() {
-  return (
-    <>
-      <Layout>
-        <Routes>
-          <Route path="/" index element={<App />} />
-          <Route path="/test" element={<AudioCard />} />
-          <Route path="/films" element={<Films/>}/>
-          <Route path="/books" element={<Books/>}/>
-        </Routes>
-      </Layout>
-    </>
-  );
+    return (
+        <>
+            <Layout>
+                {localStorage.getItem("token")  ? (
+                    <Routes>
+                        <Route path="/" index element={<AudioCard/>}/>
+                        <Route path="/test" element={<AudioCard/>}/>
+                        <Route path="/films" element={<Films/>}/>
+                        <Route path="/books" element={<Books/>}/>
+
+                    </Routes>
+                ) : (
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/login" />} />
+                        <Route path="/register" index element={<Register/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                    </Routes>
+                )}
+            </Layout>
+        </>
+    )
+        ;
 }
 
 export default App;
