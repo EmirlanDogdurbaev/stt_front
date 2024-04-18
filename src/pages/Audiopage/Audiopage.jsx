535b61eeab8ee2a665e3ecc0d3665d191f9d51ca
Aidarbek Kurmankozhoev <aidarbek.kurmankojoev.pls@gmail.com>
2024-04-18 07:00:54 +0600

    filmpage

--- src/pages/Audiopage/Audiopage.jsx
+++ src/pages/Audiopage/Audiopage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Audiopage.module.scss";
 import AudioCard from "../../components/AudioCard/AudioCard";
 import { Link } from "react-router-dom";
@@ -54,27 +54,35 @@ function Audiopage() {
       imageUrl:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3xvNtsNVxFRY_eqFm-Hmyrlqo0hEvGmqbkq5mADcWWA&s",
     },
-    // Добавьте остальные данные о песнях по аналогии
+    
   ];
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
   return (
     <div className={classes.Audiopage}>
-      <aside className={classes.Audiopage__navigation}>
-        <ul className={classes.Audiopage__links}>
-          <li className={classes.Audiopage__link}>Каталог аудиозаписей</li>
-          <li className={classes.Audiopage__link}>Свои аудиозаписи</li>
-        </ul>
-      </aside>
       <section className="Audiopage__content">
+      <input
+      accept=".mp3"
+        type="file"
+        id="fileInput"
+        style={{ display: 'none' }}
+        onChange={handleFileChange} />
+      <button onClick={() => document.getElementById('fileInput').click()} className={classes.btn }>Загрузите своё аудио</button>
+
         <h3 className={classes.Audiopage__title}>Аудиозаписи</h3>
+        {selectedFile!==null?
         <ul className={classes.Audiopage__audios}>
-          <li className={classes.Audiopage__audio}>
-            <Link to="#">
-              <audio >
-                <source src="https://soundcloud.com/21savage/21-savage-travis-scott-metro?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" type="audio/mp3"/>
-              </audio>
-            </Link>
-          </li>
-        </ul>
+        {songsData.map((item, id)=>{
+          return<div key={id}>
+            <AudioCard songName={item.songName} authorName={item.authorName} imageUrl={item.imageUrl}/>
+          </div>
+        })}
+      </ul>:<>
+        
+      </>}
       </section>
 
     </div>
